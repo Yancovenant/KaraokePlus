b0d9c4f162c3f9042bf70b2bf3cdb8d07ecf393b
Yancovenant <tirtamoto@gmail.com>
2026-07-31 16:49:42 +0700

    U

--- kplus/pipelines/refiner.py
+++ kplus/pipelines/refiner.py
@@ -15,28 +15,26 @@ class Refiner:
         self.hop_length = int((self.sr / 1000) * precision_ms)
         self.frame_length = int(self.hop_length * 2)
 
-    def _get_med_segs(self, *align_segs) -> Segment:
+    def _get_med_segs(self, ref_seg, *align_segs) -> Segment:
         env.numpy; import copy, numpy as np # type: ignore  # noqa: B018, I001
-        median_words = []
-        for words in zip(*(seg.words for seg in align_segs)):
+        # median_words = []
+        for words, r_word in zip(*(seg.words for seg in align_segs), ref_seg.words):
             med_start = np.median([w.start for w in words])
             med_end = np.median([w.end for w in words])
-            rw = copy.copy(words[0])
-            rw.start = float(med_start)
-            rw.end = float(med_end)
-            median_words.append(rw)
-        return Segment(words=median_words)
+            r_word.start = float(med_start)
+            r_word.end = float(med_end)
+        return ref_seg
 
-    def refine_timestamp(self, audio: AudioType, sr: int, *align_results, audio_segments: list[AudioSegment]) -> Result:
+    def refine_timestamp(self, audio: AudioType, sr: int, *align_results, ref_segments: Result, audio_segments: list[AudioSegment]) -> Result:
         audio_np = _process_audio(audio, sr, self.sr)
-        refined_segs = []
-        for i, (*align_segs, audio_segment) in enumerate(zip(*(res.segments for res in align_results), audio_segments)):
+        # refined_segs = []
+        for i, (*align_segs, ref_seg, audio_segment) in enumerate(zip(*(res.segments for res in align_results), ref_segments.segments, audio_segments)):
             # min_align_start = min(seg.start for seg in align_segs)
             # safe_start = min(min_align_start, audio_segment.start)
             # max_align_end = max(seg.end for seg in align_segs)
             # safe_end = max(max_align_end, audio_segment.end)
-            med_seg = self._get_med_segs(*align_segs)
-            refined_segs.append(med_seg)
-        return Result(segments=refined_segs)
+            med_seg = self._get_med_segs(ref_seg, *align_segs)
+            # refined_segs.append(med_seg)
+        return ref_segments
 
 #### More on the development later
