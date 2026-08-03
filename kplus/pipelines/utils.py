4be8860d365d49d19bef6b64acef284c432f3790
Yancovenant <tirtamoto@gmail.com>
2026-08-03 15:48:51 +0700

    U

--- kplus/pipelines/utils.py
+++ kplus/pipelines/utils.py
@@ -58,6 +58,7 @@ def convert_audio_channels(wav, channels=2):
 
 class AudioLoader:
     def __init__(self, audio: AudioType, **kwargs):
+        self.audio_path = "None"
         self._process_audio(audio, **kwargs)
 
     @cached_property
