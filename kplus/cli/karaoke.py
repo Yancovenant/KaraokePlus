97cb74cfbb61a48b1ae394a28f285bff997ea60f
Yancovenant <tirtamoto@gmail.com>
2026-08-03 16:35:28 +0700

    U

--- kplus/cli/karaoke.py
+++ kplus/cli/karaoke.py
@@ -71,7 +71,7 @@ class Karaoke(Command):
         _,audio_segments = AAD(aad_opts).get_audio_segments(sep_info.vocs_path)
         logger.info(f"Finished Getting Audio Segments -- {len(audio_segments)} segments")
         for aseg in audio_segments:
-            logger.debug(f"Segment: {aseg.h_start:.3f} - {aseg.h_end:.3f} ({aseg.duration:.3f})")
+            logger.debug(f"Segment: {aseg.h_start} - {aseg.h_end} ({aseg.duration:.3f})")
         
         # At this point i think we wanna convert the sampling rate to be 16000 since both uses that?
         # 2.5 make the audio_np available and pass it then to the rest (using sr 16KHz for now for everything)
