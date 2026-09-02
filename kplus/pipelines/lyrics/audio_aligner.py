1db67cc40d35fac6e2bfc663770d7eda17cf6fe2
root <root@localhost.localdomain>
2026-09-02 09:51:50 +0700

    u

--- kplus/pipelines/lyrics/audio_aligner.py
+++ kplus/pipelines/lyrics/audio_aligner.py
@@ -29,17 +29,8 @@ class AudioAligner:
             if len(data.audio_ids) > 0: continue
             prev_idx = next((j for j in range(i - 1, -1, -1) if datas[j].audio_ids), None,)
             next_idx = next((j for j in range(i + 1, len(datas)) if datas[j].audio_ids), None,)
-            prev_line, next_line = None, None
-            prev_gap, next_gap = 0, 0
-            for j in range(i - 1, -1, -1):
-                if (prev_line:=datas[j]).audio_ids: break
-                prev_gap += 1
-            for j in range(i + 1, len(datas)):
-                if (next_line:=datas[j]).audio_ids: break
-                next_gap += 1
-            start_idx = i - prev_gap
-            end_idx = i + next_gap
-            dropped = [datas[k] for k in range(start_idx, end_idx + 1)]
+            dropped = [datas[k] for k in range(prev_idx, next_idx + 1)]
+            rich.print(datas[next_idx].audio_ids, datas[next_idx+1].audio_ids)
             min_audio_id = max(prev_line.audio_ids) if prev_line is not None else 0
             max_audio_id = min(next_line.audio_ids) if next_line is not None else len(audiosegments) - 1
             assert min_audio_id <= max_audio_id, "This is safe to assume that there something wrong with the alignment"
@@ -66,7 +57,7 @@ class AudioAligner:
                 if max_audio_id not in data.audio_ids:
                     data.audio_ids.append(max_audio_id)
             if not is_populated(data.tokens[len(data.tokens) - 1]):
-                if i < len(data.tokens) - 1: max_audio_id = min(datas[i+1].audio_ids)
+                if i < len(datas) - 1: max_audio_id = min(datas[i+1].audio_ids)
                 else: max_audio_id = min(len(audiosegments) - 1, max(data.audio_ids) + 1)
                 min_audio_id = max(data.audio_ids)
                 min_audiosegment = audiosegments[min_audio_id]
@@ -83,6 +74,7 @@ class AudioAligner:
             min_start = min(t.start for t in data.tokens if t.start is not None)
             max_end = max(t.end for t in data.tokens if t.end is not None)
             for token in data.tokens:
+                assert t.start < t.end
                 if token.start is not None and token.end is not None: continue
                 # just interpolate it fully
                 token.start = min_start
