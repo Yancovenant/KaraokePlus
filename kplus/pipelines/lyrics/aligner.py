3ddf0e47c5a39e5f886722854f7fb94ec5d2cc40
Yancovenant <tirtamoto@gmail.com>
2026-09-17 13:11:42 +0700

    U

--- kplus/pipelines/lyrics/aligner.py
+++ kplus/pipelines/lyrics/aligner.py
@@ -1,3 +1,4 @@
+import logging
 from collections import defaultdict
 
 from kplus import env
@@ -19,6 +20,8 @@ from .utils import Tokens
 __all__ = [
     "LyricAligner"
 ]
+
+logger = logging.getLogger(__name__)
 
 class LyricAligner:
     """ Main For Lyrics Aligner """
@@ -64,59 +67,74 @@ class LyricAligner:
             start_gap = ref_tokens[min(token_ids)].start - audio_segment.start
             end_gap = audio_segment.end - ref_tokens[max(token_ids)].end
             unused_gaps[i] = (start_gap, end_gap)
-        last_word_idx = -1
 
-        for i, audio_segment in enumerate(audiosegments):
-            last_audio_id = len(audiosegments) - 1
-            if i == last_audio_id and (unknown := list(range(last_word_idx + 1, len(ref_tokens)))):
-                # This is if the last text is still left out
+        last_word_idx = -1
+        last_audio_id = len(audiosegments) - 1
+        def _check_missing(max_id: int):
+            return list(range(last_word_idx + 1, max_id))
+        for i in range(len(audiosegments)):
+            last_audio_and_still_have_word_missing = (i == last_audio_id and (missing := _check_missing(len(ref_tokens))))
+            if last_audio_and_still_have_word_missing:
+                logger.debug(f"Subject already in a last segment, but there is still missing word {missing}")
                 prev_anchor_audio_id = max(k for k, v in ref_by_audio.items() if v)
-                prev_start_gap, prev_end_gap = unused_gaps[prev_anchor_audio_id]
+                _, prev_end_gap = unused_gaps[prev_anchor_audio_id]
                 if prev_end_gap > 0.4:
-                    # Combining to previous anchor
-                    ref_by_audio[prev_anchor_audio_id] = sorted(ref_by_audio[prev_anchor_audio_id] + unknown)
+                    logger.debug(f"  Previous end gap is still large, assigned it to previous {ref_by_audio[prev_anchor_audio_id]} | {missing}")
+                    ref_by_audio[prev_anchor_audio_id] = sorted(ref_by_audio[prev_anchor_audio_id] + missing)
                 else:
+                    logger.debug("  Previous end gap insufficient, try getting the next audio")
                     if prev_anchor_audio_id < i:
-                        # Using exactly the next audio id
-                        ref_by_audio[prev_anchor_audio_id+1] = sorted(unknown)
+                        logger.debug(f"  Next audio exists, {prev_anchor_audio_id+1} | {ref_by_audio[prev_anchor_audio_id+1]}")
+                        ref_by_audio[prev_anchor_audio_id+1] = sorted(missing)
             token_ids = ref_by_audio[i]
             if not token_ids: continue
-
-            start_gap, end_gap = unused_gaps[i]
-            _, prev_end_gap = unused_gaps[i-1] if i > 0 else (None, None)
-            next_start_gap, _ = unused_gaps[i+1] if i < last_audio_id else (None, None)
-            if min(token_ids) > last_word_idx + 1 and (unknown := list(range(last_word_idx + 1, min(token_ids)))):
-                unknown_text = " ".join(ref_tokens[j].word for j in unknown)
+            there_still_prev_word_being_skipped = (min(token_ids) > last_word_idx + 1 and (missing := _check_missing(min(token_ids))))
+            if there_still_prev_word_being_skipped:
+                logger.debug(f"Subject still skipping words: {ref_by_audio[i-1]} | {missing} | {ref_by_audio[i]}")
+                missing_text = " ".join(ref_tokens[j].word for j in missing)
                 known_text = " ".join(ref_tokens[j].word for j in token_ids)
                 if prev_anchor_audio_id := next((j for j in range(i - 1, -1, -1) if ref_by_audio[j]), None):
+                    logger.debug(f"  Previous Anchor Audio Exists: {prev_anchor_audio_id}")
                     _, anchor_end_gap = unused_gaps[prev_anchor_audio_id]
                     if anchor_end_gap > 1.0:
-                        ref_by_audio[prev_anchor_audio_id] = sorted(ref_by_audio[prev_anchor_audio_id] + unknown)
-                elif similarity(unknown_text, known_text) > 0.7:
-                    # Similar in ratio.
-                    ref_by_audio[i] = sorted(token_ids + unknown)
-                else:
-                    # Extrapolate to previous audio id
-                    ref_by_audio[i] = sorted(token_ids + unknown + ref_by_audio[i-1])
-                    ref_by_audio[i-1] = sorted(token_ids + unknown + ref_by_audio[i-1])
-            token_ids = ref_by_audio[i]
-            if i < last_audio_id and (next_token_ids := ref_by_audio[i+1]):
-                if unknown := sorted(set(range(max(token_ids) + 1, min(next_token_ids)))):
-                    if next_start_gap > end_gap:
-                        # Unknown Belong to the next
-                        ref_by_audio[i+1] = sorted(token_ids + unknown)
+                        logger.debug(f"  Previous anchor gap still high assigned to previous anchor {sorted(ref_by_audio[prev_anchor_audio_id] + missing)}")
+                        ref_by_audio[prev_anchor_audio_id] = sorted(ref_by_audio[prev_anchor_audio_id] + missing)
                     else:
-                        # Unknown being merged next + current
-                        ref_by_audio[i] = sorted(token_ids + unknown + next_token_ids)
-                        ref_by_audio[i+1] = sorted(token_ids + unknown + next_token_ids)
+                        logger.debug(f"  Previous anchor gap insufficient, combining to previous audio: {ref_by_audio[i-1]} | {missing} | {token_ids}")
+                        ref_by_audio[i] = sorted(token_ids + missing + ref_by_audio[i-1])
+                        ref_by_audio[i-1] = sorted(token_ids + missing + ref_by_audio[i-1])
+                elif (ratio:=similarity(missing_text, known_text)) > 0.7:
+                    logger.debug(f"  Similarity high ({ratio}) between\n{missing_text} | {known_text}\n{sorted(token_ids + missing)}")
+                    ref_by_audio[i] = sorted(token_ids + missing)
+                else:
+                    logger.debug(f"  Combining to previous Audio: {ref_by_audio[i-1]} | {missing} | {token_ids}")
+                    ref_by_audio[i] = sorted(token_ids + missing + ref_by_audio[i-1])
+                    ref_by_audio[i-1] = sorted(token_ids + missing + ref_by_audio[i-1])
+            token_ids = ref_by_audio[i]
+            start_gap, end_gap = unused_gaps[i]
+            if i < last_audio_id and (next_token_ids := ref_by_audio[i+1]):
+                next_start_gap, _ = unused_gaps[i+1] if i < last_audio_id else (None, None)
+                if missing := sorted(set(range(max(token_ids) + 1, min(next_token_ids)))):
+                    logger.debug(f"Subject is missing: {token_ids} | {missing} | {next_token_ids}")
+                    if next_start_gap > end_gap:
+                        logger.debug(f"  Belong to the next: {sorted(next_token_ids + missing)}")
+                        ref_by_audio[i+1] = sorted(next_token_ids + missing)
+                    else:
+                        logger.debug(f"  Combine Both: {sorted(token_ids + missing + next_token_ids)}")
+                        ref_by_audio[i] = sorted(token_ids + missing + next_token_ids)
                 elif end_gap > 2.0 or next_start_gap > 1.0:
                     # No Unknown but the gap in either each segment is so long
                     # So we combine
-                    ref_by_audio[i] = sorted(token_ids + unknown + next_token_ids)
-                    ref_by_audio[i+1] = sorted(token_ids + unknown + next_token_ids)
-
+                    logger.debug(f"Subject for combine: {ref_by_audio[i]} | {ref_by_audio[i+1]}")
+                    ref_by_audio[i] = sorted(ref_by_audio[i] + ref_by_audio[i+1])
+                    ref_by_audio[i+1] = sorted(ref_by_audio[i] + ref_by_audio[i+1])
             token_ids = ref_by_audio[i]
             last_word_idx = max(token_ids)
+
+        assigned = [token_id for token_ids in ref_by_audio.values() for token_id in token_ids]
+        missing = (set(range(len(ref_tokens))) - set(assigned))
+        if missing:
+            logger.warning(f"Missing: {missing}")
 
         def should_merge(i, j) -> bool:
             a_tokens = ref_by_audio[i]
@@ -159,24 +177,11 @@ class LyricAligner:
                 langs = ["English"]
             new_texts.append(TextTiming(words=words, language=langs[0]))
         assert len(new_texts) == len(new_audiosegments)
+        new_words = [w for text in new_texts for w in text.words]
+        assert len(new_words) == len(ref_tokens)
         if env.verbose and not no_show:
             for i, (text, audio_segment) in enumerate(zip(new_texts, new_audiosegments)):
                 rich.print(f"[{i}] {audio_segment.starth} - {audio_segment.endh} ({audio_segment.duration})")
                 rich.print(f"[{i}] {text.text}")
         return ASRResult(texts=new_texts), new_audiosegments
-        # # Not Used
-        # datas = self.audio_aligner(ref_tokens, audiosegments)
-        # new_audiosegments = []
-        # results = []
-        # for data in datas:
-        #     min_audiosegment = audiosegments[min(data.audio_ids)]
-        #     max_audiosegment = audiosegments[max(data.audio_ids)]
-        #     new_audiosegments.append(AudioSegment(start=min_audiosegment.start, end=max_audiosegment.end))
-        #     results.append(data.to_texttiming())
-        # if env.verbose:
-        #     for res, aseg in zip(results, new_audiosegments):
-        #         rich.print(res.starth, res.endh, res.text)
-        #         rich.print(aseg.starth, aseg.endh)
-        #         rich.print("="*20)
-        # return ASRResult(texts=results), new_audiosegments
     
